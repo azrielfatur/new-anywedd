f1bdda0af6999afaaeeb66cbfa906600b964d250
azrielfatur <halfhour.id@gmail.com>
2022-03-13 03:01:35 +0700

    Edit image meta tags

--- pages/vian-eva.vue
+++ pages/vian-eva.vue
@@ -22,12 +22,12 @@ export default {
                 {
                     hid: 'image',
                     name: 'image',
-                    content: '_nuxt/img/thumbnail.png'
+                    content: '~/assets/img/thumbnail.png'
                 },
                 {
                     hid: 'og:image',
                     name: 'og:image',
-                    content: '_nuxt/img/thumbnail.png'
+                    content: '~/assets/img/16436960169351.webp'
                 }
             ]
         }
