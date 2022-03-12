ff9ddb5982f01a0d6349304ac715803ce67fd754
azrielfatur <halfhour.id@gmail.com>
2022-03-13 02:13:27 +0700

    Fix assets

--- components/Templates/Vian-Eva.vue
+++ components/Templates/Vian-Eva.vue
@@ -5,8 +5,8 @@
             <button class="py-2 px-4 bg-gray-100" unelevated :icon="PlayingAudio ? 'music_off' : 'music_note'" @click.prevent="PlayingAudio ? pauseMusic(FileAudio) : playMusic(FileAudio) "></button>
         </div>
         <div class="relative">
-            <img src="img/16436960169351.webp" class="w-full hidden md:block h-screen object-cover" alt="Vian & Eva">
-            <img src="img/cover-mobile.webp" class="w-full block md:hidden h-screen object-cover" alt="Vian & Eva">
+            <img src="~/assets/img/16436960169351.webp" class="w-full hidden md:block h-screen object-cover" alt="Vian & Eva">
+            <img src="~/assets/img/cover-mobile.webp" class="w-full block md:hidden h-screen object-cover" alt="Vian & Eva">
             <div class="absolute mt-10 md:mt-0 top-36 md:top-72 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
                 <div class="text-lg font-thin">The Wedding Of</div>
                 <div class="font-qwigley text-8xl lg:text-8xl">Eva <br> & <br> Vian</div>
@@ -25,13 +25,14 @@ export default {
         return {
             GuestInvited: '',
             PlayingAudio: false,
-            FileAudio: new Audio('music/Paul Anka  Put Your Head on My Shoulder Cover by The Macarons Project.mp3'),
+            FileAudio: new Audio('_nuxt/assets/music/Paul Anka  Put Your Head on My Shoulder Cover by The Macarons Project.mp3'),
 
             SpecialInvited: false,
         }
     },
     mounted() {
         this.getGuestInvited()
+        console.log(this.FileAudio)
     },
     methods: {
         getGuestInvited() {
--- components/Utils/GuestModal.vue
+++ components/Utils/GuestModal.vue
@@ -3,7 +3,7 @@
         <div class="flex fixed justify-center bg-black opacity-60 top-0 bottom-0 right-0 left-0 z-10"> 
         </div>
         <div class="flex fixed justify-center items-center top-0 bottom-0 right-0 left-0 z-10">
-                <div class="flex items-center bg-white w-96 h-48 rounded-md mx-5" style="background-image: url('img/SpecialInvited.png'); background-size: 100% 100%;">
+                <div class="flex items-center bg-white w-96 h-48 rounded-md mx-5" style="background-image: url('~assets/img/SpecialInvited.png'); background-size: 100% 100%;">
                 <div class="text-center py-5 mx-auto">
                     <p class="text-sm">Kepada Yth.</p>
                     <h6 class="text-2xl font-medium py-2">Testing Name</h6>
