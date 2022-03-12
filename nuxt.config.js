008be4c88a4fcceb2f53bc85c74881c802a0e9c3
azrielfatur <halfhour.id@gmail.com>
2022-03-13 02:29:57 +0700

    Edit meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'AnyWedd-Nuxt',
+    title: 'AnyWedd',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'asd' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
--- pages/vian-eva.vue
+++ pages/vian-eva.vue
@@ -5,7 +5,18 @@
 <script>
 import VianEva from '~/components/Templates/Vian-Eva.vue'
 export default {
+head: {
+    title: 'Pernikahan Vian & Eva',
+    meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+            hid: 'description',
+            name: 'description',
+            content: 'Undangan Pernikahan Vian & Eva'
+        }
+    ]
+},
   components: { VianEva },
-    
 }
 </script>
